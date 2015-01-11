a41a4718d0ca05d171fb3d0c9870c25fbad6efb5
fgremper <fgremper@users.noreply.github.com>
2015-01-11 20:21:21 +0100

    cleanup, moved test suite into server junit test

---
+++ RtcaServer/src/ch/ethz/fgremper/rtca/testing/TestSettings.java
@@ -0,0 +1,6 @@
+package ch.ethz.fgremper.rtca.testing;
+
+public class TestSettings {
+	static final String SANDPIT_DIRECTORY_PATH = "/Users/novocaine/Documents/masterthesis/testsandpit";
+	/* "../../testsandpit/" */
+}
