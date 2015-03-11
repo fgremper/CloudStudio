087ac112ffb753cc9691d8ccda81ec2c1df0dfc4
fgremper <fgremper@users.noreply.github.com>
2015-03-11 01:22:01 +0100

    gui look done

--- RtcaClient/src/ch/ethz/fgremper/rtca/ClientMain.java
+++ RtcaClient/src/ch/ethz/fgremper/rtca/ClientMain.java
@@ -1,5 +1,12 @@
 package ch.ethz.fgremper.rtca;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
@@ -11,6 +18,7 @@ import org.json.JSONObject;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.BevelBorder;
 
 /**
  * RTCA client main class.
@@ -27,10 +35,23 @@ public class ClientMain {
         frame.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent windowEvent){
                System.exit(0);
-            }        
+            }
          });
         
-        frame.setSize(400,400);
+        frame.setSize(600, 420);
+        frame.setResizable(false);
+
+        
+        Container pane = frame.getContentPane();
+        pane.setLayout(null);
+        
+        
+        JPanel statusPanel = new JPanel();
+        statusPanel.setBorder(BorderFactory.createBevelBorder(BevelBorder.LOWERED));
+        statusPanel.setBounds(0, 0, 600, 150);
+        //pane.add(statusPanel);
+        
+        
         
         DefaultListModel fruitsName = new DefaultListModel();
 
@@ -38,28 +59,95 @@ public class ClientMain {
         fruitsName.addElement("Grapes");
         fruitsName.addElement("Mango");
         fruitsName.addElement("Peer");
+        fruitsName.addElement("Peer");
+        fruitsName.addElement("Peer");
+        fruitsName.addElement("Peer");
+        fruitsName.addElement("Peer");
+        fruitsName.addElement("Peer");
+        fruitsName.addElement("Peer");
+        fruitsName.addElement("Peer");
+        fruitsName.addElement("Peer");
+        fruitsName.addElement("Peer");
+        fruitsName.addElement("Peer");
+        fruitsName.addElement("Peer");
+        fruitsName.addElement("Last");
 
+        JList fruitList = new JList(fruitsName);
+
+        fruitList.setFont(new Font("Helvetica", Font.PLAIN, 12));
+        
+        JScrollPane fruitListScrollPane = new JScrollPane(fruitList);
+        fruitListScrollPane.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+        fruitListScrollPane.setBounds(5, 155, 590, 237);
+        
+        // Scroll down
+        JScrollBar vertical = fruitListScrollPane.getVerticalScrollBar();
+        vertical.setValue(vertical.getMaximum());
+        
+        pane.add(fruitListScrollPane);
+        
         try {
-	        BufferedImage myPicture = ImageIO.read(new File("traffic-light-all.png"));
+	        BufferedImage myPicture = ImageIO.read(new File("TrafficGreen.png"));
 	        JLabel picLabel = new JLabel(new ImageIcon(myPicture));
-	        frame.add(picLabel);
+	        picLabel.setBounds(15, 15, 120, 120);
+	        pane.add(picLabel);
         }
         catch (Exception e) {
         	// 
         }
-        
-        JList fruitList = new JList(fruitsName);
-        
-        JScrollPane fruitListScrollPane = new JScrollPane(fruitList); 
 
-        frame.add(fruitListScrollPane);
+        JLabel monitoringText = new JLabel("Monitoring 4 local repositories.");
+        monitoringText.setFont(new Font("Helvetica", Font.PLAIN, 14));
+        monitoringText.setBounds(160, 27, 400, 20);
+        pane.add(monitoringText);
+        
+        JLabel statusCaption = new JLabel("Status:");
+        statusCaption.setFont(new Font("Helvetica", Font.BOLD, 14));
+        statusCaption.setBounds(160, 58, 100, 20);
+        pane.add(statusCaption);
+        
+        JLabel lastUpdateCaption = new JLabel("Last update:");
+        lastUpdateCaption.setFont(new Font("Helvetica", Font.BOLD, 14));
+        lastUpdateCaption.setBounds(160, 78, 100, 20);
+        pane.add(lastUpdateCaption);
+
+        JLabel nextUpdateCaption = new JLabel("Next update:");
+        nextUpdateCaption.setFont(new Font("Helvetica", Font.BOLD, 14));
+        nextUpdateCaption.setBounds(160, 109, 100, 20);
+        pane.add(nextUpdateCaption);
+        
+
+        JLabel statusText = new JLabel("Idle");
+        statusText.setFont(new Font("Helvetica", Font.PLAIN, 14));
+        statusText.setBounds(270, 58, 300, 20);
+        pane.add(statusText);
+        
+        JLabel lastUpdateText = new JLabel("49 seconds ago");
+        lastUpdateText.setFont(new Font("Helvetica", Font.PLAIN, 14));
+        lastUpdateText.setBounds(270, 78, 300, 20);
+        pane.add(lastUpdateText);
+        
+
+        JProgressBar progressBar = new JProgressBar(0, 100);
+        progressBar.setValue(80);
+        //progressBar.setStringPainted(true);
+        //progressBar.setFont(new Font("Helvetica", Font.PLAIN, 14));
+        progressBar.setBounds(270, 109, 190, 20);
+        pane.add(progressBar);
+        
+        JButton forceUpdateButton = new JButton("Force update");
+        forceUpdateButton.setFont(new Font("Helvetica", Font.PLAIN, 12));
+        forceUpdateButton.setBounds(470, 109, 100, 20);
+        pane.add(forceUpdateButton);
+        
+        //frame.add();
         
         //Add the ubiquitous "Hello World" label.
         //JLabel label = new JLabel("Hello World");
         //frame.getContentPane().add(label);
 
         //Display the window.
-        frame.pack();
+        //frame.pack();
         frame.setVisible(true);
     }
     
@@ -69,7 +157,14 @@ public class ClientMain {
 	 * @throws Exception
 	 */
 	public static void main(String[] args) {
-
+		 try {
+	            // Set System L&F
+	        //UIManager.setLookAndFeel(
+	        //    UIManager.getSystemLookAndFeelClassName());
+	    } 
+		 catch (Exception e) {
+			 
+		 }
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 createAndShowGUI();
